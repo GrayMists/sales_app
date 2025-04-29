277904d835378c5c540343bed0b6feaefb09e397
GrayMists <andrew.puliak@gmail.com>
2025-04-29 16:34:29 +0300

    допрацбована сторінка логіну

--- page/login.py
+++ page/login.py
@@ -12,25 +12,21 @@ supabase: Client = create_client(url, key)
 def show_login_data():
     st.title("Login with Supabase")
 
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
+    if "user" not in st.session_state:
+        email = st.text_input("Email")
+        password = st.text_input("Password", type="password")
 
-    if st.button("Login"):
-        try:
-            user = supabase.auth.sign_in_with_password({
-                "email": email,
-                "password": password
-            })
-            st.success(f"Logged in as: {user.user.email}")
-            st.write("Email:", user.user.email)
-            st.write("ID:", user.user.id)
-            st.write("Created at:", user.user.created_at)
-            st.write("Last sign-in at:", user.user.last_sign_in_at)
-            st.write("Повне Імʼя:", user.user.user_metadata.get("full_name", ""))
-            st.session_state["user"] = user
-        except Exception as e:
-            st.error("Login failed")
-            st.exception(e)
+        if st.button("Login"):
+            try:
+                user = supabase.auth.sign_in_with_password({
+                    "email": email,
+                    "password": password
+                })
+                
+                st.session_state["user"] = user
+            except Exception as e:
+                st.error("Login failed")
+                st.exception(e)
 
     if "user" in st.session_state and st.session_state["user"] is not None:
         current_user = st.session_state["user"]
@@ -47,23 +43,19 @@ def show_login_data():
                 "full_name": full_name
             }
 
-        if not full_name:
-            with st.form("profile_form"):
-                new_full_name = st.text_input("Full Name")
-                submitted = st.form_submit_button("Update Profile")
+        with st.form("profile_form"):
+            new_full_name = st.text_input("Full Name", value=full_name)
+            submitted = st.form_submit_button("Update Profile")
 
-                if submitted:
-                    try:
-                        supabase.auth.update_user({
-                            "data": {
-                                "full_name": new_full_name
-                            }
-                        })
-                        st.success("Profile updated!")
-                        st.session_state.user_data["full_name"] = new_full_name
-                    except Exception as e:
-                        st.error("Update failed")
-                        st.exception(e)
-        else:
-            with st.expander("Update Profile", expanded=False):
-                st.info("Профіль вже містить повне імʼя.")
+            if submitted:
+                try:
+                    supabase.auth.update_user({
+                        "data": {
+                            "full_name": new_full_name
+                        }
+                    })
+                    st.success("Profile updated!")
+                    st.session_state.user_data["full_name"] = new_full_name
+                except Exception as e:
+                    st.error("Update failed")
+                    st.exception(e)
