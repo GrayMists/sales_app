ee4a26940993e52520398418cb204fc06e114f2d
GrayMists <andrew.puliak@gmail.com>
2025-04-29 16:30:55 +0300

    оновлено картку користувача

--- page/login.py
+++ page/login.py
@@ -32,29 +32,38 @@ def show_login_data():
             st.error("Login failed")
             st.exception(e)
 
-    if "user" in st.session_state:
+    if "user" in st.session_state and st.session_state["user"] is not None:
         current_user = st.session_state["user"]
         st.subheader("User Profile")
 
+        full_name = current_user.user.user_metadata.get("full_name", "")
+        email = current_user.user.email
+        st.write("Повне Імʼя:", full_name if full_name else "не вказано")
+        st.write("Email:", email)
+
         if "user_data" not in st.session_state:
             st.session_state.user_data = {
-                "email": current_user.user.email,
-                "full_name": current_user.user.user_metadata.get("full_name", "")
+                "email": email,
+                "full_name": full_name
             }
 
-        with st.form("profile_form"):
-            new_full_name = st.text_input("Full Name", value=st.session_state.user_data["full_name"])
-            submitted = st.form_submit_button("Update Profile")
+        if not full_name:
+            with st.form("profile_form"):
+                new_full_name = st.text_input("Full Name")
+                submitted = st.form_submit_button("Update Profile")
 
-            if submitted:
-                try:
-                    supabase.auth.update_user({
-                        "data": {
-                            "full_name": new_full_name
-                        }
-                    })
-                    st.success("Profile updated!")
-                    st.session_state.user_data["full_name"] = new_full_name
-                except Exception as e:
-                    st.error("Update failed")
-                    st.exception(e)
+                if submitted:
+                    try:
+                        supabase.auth.update_user({
+                            "data": {
+                                "full_name": new_full_name
+                            }
+                        })
+                        st.success("Profile updated!")
+                        st.session_state.user_data["full_name"] = new_full_name
+                    except Exception as e:
+                        st.error("Update failed")
+                        st.exception(e)
+        else:
+            with st.expander("Update Profile", expanded=False):
+                st.info("Профіль вже містить повне імʼя.")
